e48287509d6006fd8c5f1102b5122182fd25e17e
Tejaswi Chaudhari <tejschaudhari522002@gmail.com>
2021-05-20 16:20:36 +0530

    Cards and History database setup-1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "accounts.apps.AccountsConfig",
     "rest_framework_simplejwt.token_blacklist",
+    "core.apps.CoreConfig",
 ]
 
 MIDDLEWARE = [
---
+++ backend/core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import CardAPIView, HistoryAPIView
+
+urlpatterns = [
+    path("api/card/", CardAPIView.as_view(), name="card_view"),
+    path("api/history/", HistoryAPIView.as_view(), name="history_view"),
+]
