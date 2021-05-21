50a8b0493f9b9d66d3c4640e5deac63346117f99
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-05-21 15:31:20 +0530

    Database updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import {React, useState, useEffect} from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Header, Welcome, Contact, Review, History, Practice, Audioinput, Mic, Signup, Signin, Footer} from './components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from "axios";
 
 function App() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    loadCards();
+  }, []);
+  const loadCards = async () => {
+    const result = await axios.get("http://127.0.0.1:8000/api/card/");
+    console.log(result.data);
+    setCards(result.data);
+    
+  }
   return (
     <Router>
       <div className="app">
@@ -24,7 +37,7 @@ function App() {
           </Route>
           <Route exact path="/exercise">
             <Header />
-            <Practice />
+            <Practice cards={cards}/>
             <Footer />
           </Route>
           <Route exact path="/audioinput">
--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import {Nav} from 'react-bootstrap';
-import web from "../Images/sample.jpg";
 import Button from 'react-bootstrap/Button';
 import "./Card.css";
 import { LinkContainer } from "react-router-bootstrap";
 
-const Card=()  => {
+const Card=({card})  => {
+  
     return(<>
           <div className="col-md-4 col-10 mx-auto">
             <div class="card" >
-              <img src={web} class="card-img-top" alt="..."/>
+              <img src={card.thumbnail} class="card-img-top" alt="..."/>
               <div class="card-body">
-                <h5 class="card-title ">Breathing Exercises</h5>
+                <h5 class="card-title ">{card.exercise_name}</h5>
                 <hr></hr>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="card-text">{card.description}</p>
                 <LinkContainer to="/audioinput">
                     <Nav.Link className="rd_activity"><Button variant="primary" className="b_activity">Start Practice</Button></Nav.Link>
                 </LinkContainer>
