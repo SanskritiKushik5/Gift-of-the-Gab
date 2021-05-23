7695103d051e6624ad299f2e9cd1bf1a86a19eb4
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-05-23 20:27:54 +0530

    Added media section

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -141,3 +141,5 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "accounts.Customer"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
--- backend/core/models.py
+++ backend/core/models.py
@@ -3,10 +3,10 @@ from accounts.models import Customer
 
 class Card(models.Model):
     exercise_name = models.CharField(max_length=500)
-    thumbnail = models.ImageField()
+    thumbnail = models.ImageField(upload_to="card/images", default="")
     description = models.TextField()
     instructions = models.TextField()
-    audio = models.FileField()
+    audio = models.FileField(upload_to="card/audio", default="")
     duration = models.DurationField()
 
     def __str__(self):
@@ -16,7 +16,7 @@ class History(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     date_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     exercise_name = models.CharField(max_length=100)
-    thumbnail = models.ImageField()
+    thumbnail = models.ImageField(upload_to="card/images", default="")
     description = models.TextField()
 
     def __str__(self):
