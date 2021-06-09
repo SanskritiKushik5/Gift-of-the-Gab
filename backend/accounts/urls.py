dd882347258d59936ba161af67e92374f8ca0dd9
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-06-09 21:09:50 +0530

    User Retrieval

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,10 +3,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import RegisterAPIView, LogOutAPIView, current_user
+from .views import RegisterAPIView, LogOutAPIView, CurrentUserAPIView
 
 urlpatterns = [
-    path('current_user/', current_user),
+    path('api/current_user/', CurrentUserAPIView.as_view(), name="current_user"),
     path("api/login/", TokenObtainPairView.as_view(), name="login_view"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh_view"),
     path("api/logout/", LogOutAPIView.as_view(), name="logout_view"),
