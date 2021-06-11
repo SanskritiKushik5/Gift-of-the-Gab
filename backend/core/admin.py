95c038400c8c91573efedc998640bd9cf1f09378
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-06-11 09:37:20 +0530

    User details fetched

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-<<<<<<< HEAD
-
-# Register your models here.
-=======
 from .models import Card, History
 
 # Register your models here.
 admin.site.register(Card)
 admin.site.register(History)
->>>>>>> upstream/main
