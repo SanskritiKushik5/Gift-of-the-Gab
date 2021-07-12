9d0fc31e98b4567126e21d8988f39233e898a724
Tejaswi-Chaudhari <tejschaudhari522002@gmail.com>
2021-07-12 12:38:56 +0530

    dash in progress

--- frontend/src/components/dash/Dashboard.jsx
+++ frontend/src/components/dash/Dashboard.jsx
@@ -7,13 +7,16 @@ import Col from 'react-bootstrap/Col';
 import { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import moment from 'moment'
 
 const Dashboard = ()  => {
 
   const { id } = useParams();
   let streak = [];
   var week = {};
-  const [active, setActive] = useState(false);
+  // const [active, setActive] = useState(false);
+  const [final, setFinal] = useState({});
+  var active = false
   var today = new Date();
   var fday = today.getDay();
   const ftoday = JSON.stringify(today).slice(1,11);
@@ -26,31 +29,52 @@ const Dashboard = ()  => {
   const loadWeekstreak = async () => {
     const result = await axios.get(`http://127.0.0.1:8000/api/weekstreak/${id}`);
     streak = result.data;
-    streak.forEach(function(item) {
-      item.date_time = item.date_time.slice(0,10)
-      console.log(item.date_time)
-      if (item.date_time === ftoday) {
-        console.log("hey we entered the if")
-        setActive(true);
-      }
-    });
+    
+      for(let i=0; i<=fday; i++){
+        console.log(i)
+        console.log("hey entered the for")
+        var wdate = moment().subtract('days', fday-i).format('YYYY-MM-DD');
+        console.log(wdate)
+        console.log(streak)
+          streak.forEach(function(item) {
+            item.date_time = item.date_time.slice(0,10)
+              if (item.date_time === wdate) {
+                console.log("hey we entered the if")
+                // setActive(true);
+                active=true
+                week[i] = active;
+                console.log("i ",i,"active ",active,"week ",week)
+                return;
+              }
+              else{
+                // setActive(false);
+                active=false
+              }
+            });
+          // week[i] = active;
+        }
+      console.log(week)
+      setFinal(week)
   }
 
-  week[fday] = active;
-  console.log(week)
-  console.log(active)
+  console.log(final)
+  // var seven = moment().subtract('days', 7).format('YYYY-MM-DD')
+  // console.log(seven)
+  // week[fday] = active;
+  // console.log(week)
+  // console.log(active)
 
   return(<>
 <div className="CSS">
   <h3>Hey, Welcome back!</h3>
 <center>
-  <button style={{backgroundColor: week[1]? "#001457" : "#b2ecfd"}} type="button" id="1" className="btn btn-circle btn-xl">Mon</button>
-  <button style={{backgroundColor: week[2]? "#001457" : "#b2ecfd"}} type="button" id="2" className="btn btn-circle btn-xl">Tue</button>
-  <button style={{backgroundColor: week[3]? "#001457" : "#b2ecfd"}} type="button" id="3" className="btn btn-circle btn-xl">Wed</button>
-  <button style={{backgroundColor: week[4]? "#001457" : "#b2ecfd"}} type="button" id="4" className="btn btn-circle btn-xl">Thu</button>
-  <button style={{backgroundColor: week[5]? "#001457" : "#b2ecfd"}} type="button" id="5" className="btn btn-circle btn-xl">Fri</button>
-  <button style={{backgroundColor: week[6]? "#001457" : "#b2ecfd"}} type="button" id="6" className="btn btn-circle btn-xl">Sat</button>
-  <button style={{backgroundColor: week[7]? "#001457" : "#b2ecfd"}} type="button" id="7" className="btn btn-circle btn-xl">Sun</button>
+  <button style={{backgroundColor: final[0]? "#001457" : "#b2ecfd"}} type="button" id="0" className="btn btn-circle btn-xl">Sun</button>
+  <button style={{backgroundColor: final[1]? "#001457" : "#b2ecfd"}} type="button" id="1" className="btn btn-circle btn-xl">Mon</button>
+  <button style={{backgroundColor: final[2]? "#001457" : "#b2ecfd"}} type="button" id="2" className="btn btn-circle btn-xl">Tue</button>
+  <button style={{backgroundColor: final[3]? "#001457" : "#b2ecfd"}} type="button" id="3" className="btn btn-circle btn-xl">Wed</button>
+  <button style={{backgroundColor: final[4]? "#001457" : "#b2ecfd"}} type="button" id="4" className="btn btn-circle btn-xl">Thu</button>
+  <button style={{backgroundColor: final[5]? "#001457" : "#b2ecfd"}} type="button" id="5" className="btn btn-circle btn-xl">Fri</button>
+  <button style={{backgroundColor: final[6]? "#001457" : "#b2ecfd"}} type="button" id="6" className="btn btn-circle btn-xl">Sat</button>
 </center>
 <div className="card">
 <Row>
