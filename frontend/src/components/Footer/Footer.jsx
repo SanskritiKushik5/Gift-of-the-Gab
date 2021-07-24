505f005dc898d6bcabfd186636cd540d9b7c126d
shrutigavali <gavalishruti21@gmail.com>
2021-07-24 19:14:56 +0530

    modified

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer class="page-footer font-small">
         <div class="footer-copyright text-center py-3">
-        © 2021 Copyright:<p>Gift of the Gab</p>
+        ©2021 Copyright:<p>Gift of the Gab</p>
         </div>
     </footer>
     )
